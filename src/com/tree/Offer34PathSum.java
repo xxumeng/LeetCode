8ed804b0c2bebe2ea811cba2c7015048d8d126b8
xxumeng <xxu_meng@163.com>
2020-12-09 19:17:45 +0800

    回溯

--- src/com/tree/Offer34PathSum.java
+++ src/com/tree/Offer34PathSum.java
@@ -1,25 +1,28 @@
 package com.tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Offer34PathSum {
-    List<List<Integer>> res = new ArrayList<>();
-    public List<Integer> trans(TreeNode root, int sum, List<Integer> tmp){
-        if (root==null && sum!=0) return null;
-        if(root==null && sum==0) {
-            res.add(tmp);
-            return tmp;
+//    插入、删除操作频繁，array也可以，都是在尾部插入删除
+//    注意终止条件
+    List<List<Integer>> res = new LinkedList<>();
+    List<Integer> track = new ArrayList<>();
+    public void backtrack(TreeNode root, int sum){
+        if (root==null) return;
+        sum -=root.val;
+        track.add(root.val);
+        if(sum==0 && root.left==null && root.left==null) {
+            res.add(new LinkedList(track));
         }
-        sum -= root.val;
-        tmp.add(root.val);
-        trans(root.left, sum, tmp);
-        trans(root.right, sum, tmp);
-        return tmp;
+        backtrack(root.left, sum);
+        backtrack(root.right, sum);
+        track.remove(track.size()-1);
     }
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        List<Integer> tmp = new ArrayList<>();
-        trans(root, sum, tmp);
+
+        backtrack(root, sum);
         return res;
     }
 }
