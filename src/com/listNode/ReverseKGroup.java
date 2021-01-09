5ceb98a89af9cfe9ba303aa20b0f990b526fa172
xxumeng <xxu_meng@163.com>
2021-01-09 16:27:52 +0800

    Leetcode25分组反转链表

---
+++ src/com/listNode/ReverseKGroup.java
@@ -0,0 +1,30 @@
+package com.listNode;
+
+public class ReverseKGroup {
+//    反转[a,b)区间，返回新的头
+    public ListNode reverse(ListNode a, ListNode b){
+        ListNode pre=null, behind= null;
+        while(a!=b){
+            behind = a.next;
+            a.next= pre;
+            pre = a;
+            a = behind;
+        }
+        return pre;
+    }
+
+
+//    k个一组递归调用反转[a,b)区间，使得前一个的尾部指向后一个的头部
+    public ListNode reverseKGroup(ListNode head, int k){
+        if (head==null) return null;
+        if (k<=1) return head;
+        ListNode b = head;
+        for(int i=0; i<k; i++){
+            if (b==null) return head;
+            b = b.next;
+        }
+        ListNode rHead = reverse(head, b);
+        head.next = reverseKGroup(b, k);
+        return rHead;
+    }
+}
