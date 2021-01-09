9c7573dfcffa23d3301de589c647a33ae9cba436
xxumeng <xxu_meng@163.com>
2021-01-09 16:28:58 +0800

    Leetcode234 回文链表，快慢指针反转后半部分链表

---
+++ src/com/listNode/Palindrome.java
@@ -0,0 +1,26 @@
+package com.listNode;
+
+public class Palindrome {
+    public ListNode reverse(ListNode head){
+        ListNode pre = null, nxt = null;
+        while(head!=null){
+            nxt = head.next;
+            head.next = pre;
+            pre = head;
+            head = nxt;
+        }
+        return pre;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode low = head, fast = head;
+        while(fast!=null && fast.next!=null){
+            low = low.next;
+            fast = fast.next.next;
+        }
+        ListNode rList = reverse(low);
+        while(head!=null && rList!=null){
+            if (head.val!=rList.val) return false;
+        }
+        return true;
+    }
+}
